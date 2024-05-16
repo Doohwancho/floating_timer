531ed60edefedf35ecde67074b01a9e3d2cbc8df
Doohwancho <doohwancho1993@gmail.com>
2024-05-16 15:18:34 +0900

    init project

---
+++ minimal_timer/minimal_timer/minimal_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  minimal_timerApp.swift
+//  minimal_timer
+//
+//  Created by cho-cho on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct minimal_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ minimal_timer/minimal_timerUITests/minimal_timerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  minimal_timerUITestsLaunchTests.swift
+//  minimal_timerUITests
+//
+//  Created by cho-cho on 5/16/24.
+//
+
+import XCTest
+
+final class minimal_timerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
